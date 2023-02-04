639823872aaea242cfae8087b996fc94f8cc4de0
Daniel <dursuaf@gmail.com>
2023-02-04 14:44:06 +0100

    Add footer files

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -1,1 +1,27 @@
+/* eslint-disable no-unused-vars */
+import { Component } from '../component/component';
+import './header.scss';
 
+export class Header extends Component {
+  constructor(public selector: string, public title: string = 'Pokepedia') {
+    super();
+    this.template = this.createTemplate();
+    this.render('beforeend');
+  }
+
+  private createTemplate() {
+    return `
+           <footer>
+           <img src="../../pikachu.png" alt="A draw of the Pikachu's face" />
+            </footer>
+    `;
+  }
+
+  render(place: globalThis.InsertPosition) {
+    super.render(place);
+  }
+}
+
+<footer>
+  <img src="../../pikachu.png" alt="" />
+</footer>;
