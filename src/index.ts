21494e809a5baa0d5103a24be9e8e5e58b25172e
Daniel <dursuaf@gmail.com>
2023-02-04 18:27:25 +0100

    Fix sass problems

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-new */
+import { Card } from './components/card/card';
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
 
 new Header('.root');
+new Card('.header');
 new Footer('.root');
